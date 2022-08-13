df54dcacc75127ae773f957f6b0461719ab3dea4
Yosua <yosua.halim@gmail.com>
2022-08-13 14:07:22 +0700

    init

---
+++ fast-api/main.py
@@ -0,0 +1,86 @@
+import json
+import consumers
+from fastapi import FastAPI, Request, Response
+from fastapi.middleware.cors import CORSMiddleware
+from redis_om import get_redis_connection, HashModel
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+redis = get_redis_connection(
+    host="redis-18644.c1.ap-southeast-1-1.ec2.cloud.redislabs.com",
+    port=18644,
+    password="abtiJaUSUmjTGjdBm4JZ91obw2P8BMDO",
+    decode_responses=True
+)
+
+
+class Delivery(HashModel):
+    budget: int = 0
+    notes: str = ''
+
+    class Meta:
+        database = redis
+
+
+class Event(HashModel):
+    delivery_id: str = None
+    type: str
+    data: str
+
+    class Meta:
+        database = redis
+
+
+@app.get("/deliveries/{pk}/status")
+async def get_state(pk: str):
+    state = redis.get(f'delivery:{pk}')
+
+    if state is not None:
+        return json.loads(state)
+
+    state = build_state(pk)
+    redis.set(f'delivery:{pk}', json.dumps(state))
+    return state
+
+
+def build_state(pk: str):
+    pks = Event.all_pks()
+    all_event = [Event.get(pk) for pk in pks]
+    events = [event for event in all_event if event.delivery_id == pk]
+    state = {}
+
+    for event in events:
+        state = consumers.CONSUMERS[event.type](state, event)
+
+    return events
+
+
+@app.post('/deliveries/create')
+async def create(request: Request):
+    body = await request.json()
+    delivery = Delivery(budget=body['data']
+                        ['budget'], notes=body['data']['notes']).save()
+    event = Event(delivery_id=delivery.pk, type=body['type'],
+                  data=json.dumps(body['data'])).save()
+    state = consumers.CONSUMERS[event.type]({}, event)
+    redis.set(f'delivery:{delivery.pk}', json.dumps(state))
+    return state
+
+
+@app.post('/event')
+async def dispatch(request: Request):
+    body = await request.json()
+    delivery_id = body['delivery_id']
+    event = Event(delivery_id=delivery_id,
+                  type=body['type'], data=json.dumps(body['data'])).save()
+    state = await get_state(delivery_id)
+    new_state = consumers.CONSUMERS[event.type](state, event)
+    redis.set(f'delivery:{delivery_id}', json.dumps(new_state))
+    return new_state
